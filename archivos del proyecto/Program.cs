4c90b09d025ea9f3e16e47c06a72c1db962f5aac
yermi-m <yermiamz@gmail.com>
2025-07-13 16:21:55 -0400

    Add files via upload

---
+++ archivos del proyecto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BibliotecaDeLosMetodos;
+
+namespace CalculadoraCientifica_v2
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.Title = "Calculadora Cientifica Basica";
+			string entrada = "";
+
+			do
+			{
+				Console.Clear();
+
+                Console.WriteLine("\nIngrese la expresion matematica: ");
+				Console.WriteLine($"            Expresion ingresada: {Metodos.PreformatearExpresion(entrada)}");
+				Console.WriteLine($"                      Resultado: {Metodos.CalcularExpresion(entrada)}");
+				Console.WriteLine("\nNomenclatura de una expresion matematica válida: -A+(-B)+(A*B).");
+				Console.WriteLine("Esta calculadora soporta lo siguiente: ");
+				Console.WriteLine("· operaciones en parentesis \n· operaciones con numeros de signo negativo (-) \n· multiplicacion (*) \n· division (/) \n· suma (+) \n· resta (-).");
+				Console.WriteLine("");
+
+				Console.SetCursorPosition(33, 1);
+				entrada = Console.ReadLine();
+				
+			} while (true);
+		}
+	}
+}
